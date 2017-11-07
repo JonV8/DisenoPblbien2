7663d69a30643452016572eccabd54920f2e91c6
JonV8 <jonfernandez92@gmail.com>
2017-11-07 09:50:08 +0100

    version1

--- app/src/main/java/com/example/peperoni/disenopbl/fragventas/AdaptadorConsolas.java
+++ app/src/main/java/com/example/peperoni/disenopbl/fragventas/AdaptadorConsolas.java
@@ -1,94 +1,62 @@
 package com.example.peperoni.disenopbl.fragventas;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.StringRequest;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
-public class AdaptadorConsolas extends BaseAdapter{
+import com.example.peperoni.disenopbl.R;
+
+import java.util.List;
+
+
+public class AdaptadorConsolas extends RecyclerView.Adapter<AdaptadorConsolas.ConsolasViewHolder>{
     private TabConsolas misConsolas;
-    private JSONArray arrayFotos;
-    private RequestQueue colaPeticiones;
-    private StringRequest strPeticion;
-    private int actual;
-
-    public AdaptadorConsolas(TabConsolas misConsolas) {
-        this.misConsolas = misConsolas;
-
-    }
-
-    @Override
-    public int getCount() {
-        return 0;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int position, View miView, ViewGroup parent) {
-        obtenerMarcas();
-        return null;
-    }
+    private List<CatConsolas> items;
 
 
-    private void obtenerMarcas() {
-        JsonArrayRequest jsonPeticion;
-        String url="http://192.168.6.188/wsPBL/servicio.php";
-        jsonPeticion=new JsonArrayRequest(Request.Method.POST, url, null,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        arrayFotos=response;
-                        //MOSTRAR EL PRIMERO Y ACTIVAR LOS BOTONES
-                        //controlar si viene vacio
-                        if(arrayFotos.length()>0) {
-                            actual = 0;
-                            mostrarDatos();
-                        }
-                        else{
-                            Toast.makeText(misConsolas.getContext(), "No hay datos que mostrar", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(misConsolas.getContext(), "Error de red", Toast.LENGTH_SHORT).show();
-                    }
-                });
-        colaPeticiones.add(jsonPeticion);
-    }
 
-    private void mostrarDatos() {
-        JSONObject foto;
-        try {
-            foto= arrayFotos.getJSONObject(actual);
-            //textId.setText(foto.getString("foto"));
-            //textMarca.setText(marca.getString("nombre"));
-        } catch (JSONException e) {
-            e.printStackTrace();
+    public static class ConsolasViewHolder extends RecyclerView.ViewHolder{
+        public ImageView imagen;
+        public TextView nombre;
+        public TextView precio;
+
+        public ConsolasViewHolder(View v) {
+            super(v);
+            imagen = (ImageView)v.findViewById(R.id.imagen);
         }
+        }
+    public AdaptadorConsolas(List<CatConsolas> items) {
+        this.items = items;
+    }
+
+    @Override
+    public ConsolasViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(misConsolas.getContext())
+                .inflate(R.layout.tabconsolas_fragment, viewGroup, false);
+        return new ConsolasViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ConsolasViewHolder viewHolder, int i) {
+        viewHolder.imagen.setImageResource(items.get(i).getImagen());
+        viewHolder.nombre.setText(items.get(i).getNombre());
+        viewHolder.precio.setText("Precio: "+items.get(i).getPrecio());
 
     }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+
+
+
+
 }
+
 
--- app/src/main/java/com/example/peperoni/disenopbl/fragventas/TabConsolas.java
+++ app/src/main/java/com/example/peperoni/disenopbl/fragventas/TabConsolas.java
@@ -3,6 +3,7 @@ package com.example.peperoni.disenopbl.fragventas;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,14 +11,22 @@ import android.widget.GridView;
 
 import com.example.peperoni.disenopbl.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TabConsolas extends Fragment {
-        private static final String TAG = "TabConsolas";
+    private RecyclerView recycler;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager lManager;
+    private static final String TAG = "TabConsolas";
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tabconsolas_fragment,container,false);
-        GridView catConsolas= (GridView) view.findViewById(R.id.catConsolas);
-        catConsolas.setAdapter(new AdaptadorConsolas(this));
+        List items = new ArrayList();
+
+
         return view;
     }
 }
